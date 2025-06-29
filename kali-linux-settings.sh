13f56f5022fca014a626e8e82eef2cae170f07a1
Serg-ux <github.stingray458@passmail.net>
2025-06-29 16:48:58 +0200

    group github repositories in a folder

--- kali-linux-settings.sh
+++ kali-linux-settings.sh
@@ -110,5 +110,38 @@ dconf write /org/gnome/terminal/legacy/profiles:/default/background-color "'#000
 dconf write /org/gnome/terminal/legacy/profiles:/default/background-darkness "0.8"
 
 # Clone exploit-database repository
-git clone https://github.com/offensive-security/exploit-databas
+git clone https://github.com/offensive-security/exploit-database.git
+
+# Create a folder named pentest_tools
+mkdir -p ~/pentest_tools
+cd ~/pentest_tools
+
+# Clone the repositories into the pentest_tools folder
+
+# Download PEAS for privilege escalation in Windows and Linux
+git clone https://github.com/peass-ng/PEASS-ng
+# SSH
+git clone https://github.com/jtesta/ssh-audit.git
+# Oracle-Tools
+git clone https://github.com/jtesta/ssh-audit.git
+  sudo apt-get install libaio1 python3-dev alien -y
+  git clone https://github.com/quentinhardy/odat.git
+  cd odat/
+  git submodule init
+  git submodule update
+  wget https://download.oracle.com/otn_software/linux/instantclient/2112000/instantclient-basic-linux.x64-21.12.0.0.0dbru.zip
+  unzip instantclient-basic-linux.x64-21.12.0.0.0dbru.zip
+  wget https://download.oracle.com/otn_software/linux/instantclient/2112000/instantclient-sqlplus-linux.x64-21.12.0.0.0dbru.zip
+  unzip instantclient-sqlplus-linux.x64-21.12.0.0.0dbru.zip
+  export LD_LIBRARY_PATH=instantclient_21_12:$LD_LIBRARY_PATH
+  export PATH=$LD_LIBRARY_PATH:$PATH
+  pip3 install cx_Oracle
+  sudo apt-get install python3-scapy -y
+  sudo pip3 install colorlog termcolor passlib python-libnmap
+  sudo apt-get install build-essential libgmp-dev -y
+  pip3 install pycryptodome
+
+git clone https://github.com/lanmaster53/recon-ng.git
+cd recon-ng
+pip3 install -r requirements.txt
 
